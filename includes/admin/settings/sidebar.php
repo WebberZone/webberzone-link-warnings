e1e186b80035bf6da8866ff5a0eed5cfa96b0df8
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2026-03-04 23:16:23 +0400

    Add ABSPATH protection to sidebar.php

--- includes/admin/settings/sidebar.php
+++ includes/admin/settings/sidebar.php
@@ -5,6 +5,9 @@
  * @package WebberZone\Link_Warnings
  */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 ?>
 <div class="postbox-container">
 	<div id="qlinksdiv" class="postbox meta-box-sortables">

20eb7efb54fb029745e5d3ffb1b3f92fab560ac7
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2026-06-13 20:26:37 +0400

    sync: update Settings API framework from canonical (unique_field repeater support, sensitive-field sanitization hardening, @package cleanup)

--- includes/admin/settings/class-settings-api.php
+++ includes/admin/settings/class-settings-api.php
@@ -5,7 +5,7 @@
  * Functions to register, read, write and update settings.
  * Portions of this code have been inspired by Easy Digital Downloads, WordPress Settings Sandbox, WordPress Settings API class, etc.
  *
- * @package    WebberZone\Link_Warnings
+ * @package WebberZone\Link_Warnings
  */
 
 namespace WebberZone\Link_Warnings\Admin\Settings;
--- includes/admin/settings/class-settings-wizard-api.php
+++ includes/admin/settings/class-settings-wizard-api.php
@@ -5,7 +5,7 @@
  * A reusable API class for creating multi-step settings wizards.
  * This class provides the framework for creating guided setup experiences.
  *
- * @package    WebberZone\Link_Warnings
+ * @package WebberZone\Link_Warnings
  */
 
 namespace WebberZone\Link_Warnings\Admin\Settings;
